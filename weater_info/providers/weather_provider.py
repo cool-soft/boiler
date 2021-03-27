6fdd05351263969dbbd8aa0d6a3321d2981f7ac3
Georgij <netbay.georgij@gmail.com>
2021-03-27 05:22:51 +0500

    Заменяем weather_provider на weather_repository

--- weater_info/providers/weather_provider.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-class WeatherProvider:
-
-    def get_weather(self,
-                    start_datetime: pd.Timestamp = None,
-                    end_datetime: pd.Timestamp = None) -> pd.DataFrame:
-        raise NotImplementedError
