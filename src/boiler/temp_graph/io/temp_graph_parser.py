0d72c6fa7cf33f85ef30a322b86c31e4f8ea0844
Georgij <netbay.georgij@gmail.com>
2021-04-25 17:34:52 +0500

    Удаляем ненужные функции (старые) для работы с temp_graph

--- src/boiler/temp_graph/io/temp_graph_parser.py
+++
@@ -1,9 +0,0 @@
-from typing import Union, IO
-
-import pandas as pd
-
-
-class TempGraphParser:
-
-    def parse_temp_graph(self, data: Union[str, IO]) -> pd.DataFrame:
-        raise NotImplementedError
