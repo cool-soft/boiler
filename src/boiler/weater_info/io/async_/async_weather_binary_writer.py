b57ffe047b2dec950be5318fffe637fde627205c
Georgij <netbay.georgij@gmail.com>
2021-04-25 15:13:01 +0500

    Добавляем async адаптер для чтения Weather при помощи sync text reader

--- src/boiler/weater_info/io/async_/async_weather_binary_writer.py
+++
@@ -1,9 +0,0 @@
-from typing import BinaryIO
-
-import pandas as pd
-
-
-class AsyncWeatherBinaryWriter:
-
-    async def write_weather_to_binary_io(self, binary_io: BinaryIO, weather_df: pd.DataFrame) -> None:
-        raise NotImplementedError
