c02ce944a091ba962dff996221c6cb9858e2982e
Georgij <netbay.georgij@gmail.com>
2021-04-30 00:53:19 +0500

    ~~~

--- tests/unittests/test_heating_obj_sync_dir_repo.py
+++ tests/unittests/test_heating_obj_sync_dir_repo.py
@@ -1,8 +1,10 @@
 import pytest
 
+from boiler.constants import column_names
 from boiler.heating_obj.io.sync.sync_heating_obj_pickle_reader import SyncHeatingObjPickleReader
 from boiler.heating_obj.io.sync.sync_heating_obj_pickle_writer import SyncHeatingObjPickleWriter
 from boiler.heating_obj.repository.sync.sync_heating_obj_dir_repository import SyncHeatingObjDirRepository
+from boiler.data_processing.processing_algo.beetween_filter_algorithm import FullClosedBetweenFilterAlgorithm
 from unittests.heating_obj_sync_repo_testing import HeatingObjSyncRepoTesting
 
 
@@ -19,10 +21,15 @@ class TestHeatingObjectSyncDirRepo(HeatingObjSyncRepoTesting):
         return SyncHeatingObjPickleWriter()
 
     @pytest.fixture
-    def repository(self, reader, writer, tmpdir):
+    def filter_algorithm(self):
+        return FullClosedBetweenFilterAlgorithm(column_name=column_names.TIMESTAMP)
+
+    @pytest.fixture
+    def repository(self, reader, writer, tmpdir, filter_algorithm):
         return SyncHeatingObjDirRepository(
             dir_path=tmpdir,
             filename_ext=self.filename_ext,
             reader=reader,
-            writer=writer
+            writer=writer,
+            filter_algorithm=filter_algorithm
         )
