959dc9a935fd80dc2fcbc0ec916caeb09307bb7b
Georgij <netbay.georgij@gmail.com>
2021-05-04 07:40:09 +0500

    Добавляем поддержку разделителя в csv reader/writer

--- src/boiler/temp_graph/io/sync_temp_graph_csv_writer.py
+++ src/boiler/temp_graph/io/sync_temp_graph_csv_writer.py
@@ -1,4 +1,3 @@
-import io
 import logging
 from typing import BinaryIO
 
@@ -10,19 +9,21 @@ from boiler.temp_graph.io.abstract_sync_temp_graph_writer import AbstractSyncTem
 class SyncTempGraphCSVWriter(AbstractSyncTempGraphWriter):
 
     def __init__(self,
-                 encoding: str = "utf-8"
+                 encoding: str = "utf-8",
+                 separator: str = ";"
                  ) -> None:
         self._logger = logging.getLogger(self.__class__.__name__)
         self._logger.debug("Creating instance")
 
         self._encoding = encoding
+        self._separator = separator
 
         self._logger.debug(f"Encoding is {encoding}")
+        self._logger.debug(f"Separator is {separator}")
 
     def write_temp_graph_to_binary_stream(self,
                                           binary_stream: BinaryIO,
                                           temp_graph_df: pd.DataFrame) -> None:
         self._logger.debug("Storing temp graph")
-        with io.TextIOWrapper(binary_stream, encoding=self._encoding) as text_stream:
-            temp_graph_df.to_csv(text_stream, index=False)
+        temp_graph_df.to_csv(binary_stream, encoding=self._encoding, sep=self._separator, index=False)
         self._logger.debug("Temp graph is stored")
