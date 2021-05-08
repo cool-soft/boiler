6abc18937ea07d659f2e5a658cea9a787d702d50
Georgij <netbay.georgij@gmail.com>
2021-05-09 02:58:37 +0500

    Добавляем тесты для Temp Requirements Predictor

--- src/boiler/temp_requirements/predictors/temp_graph_requirements_predictor.py
+++ src/boiler/temp_requirements/predictors/temp_graph_requirements_predictor.py
@@ -1,46 +1,51 @@
 import logging
-from typing import Dict
+from typing import Tuple
 
 import pandas as pd
-from .abstract_temp_requirements_predictor import AbstractTempRequirementsPredictor
+
 from boiler.constants import column_names
 from boiler.data_processing.float_round_algorithm import AbstractFloatRoundAlgorithm
+from .abstract_temp_requirements_predictor import AbstractTempRequirementsPredictor
 
 
 class TempGraphRequirementsPredictor(AbstractTempRequirementsPredictor):
 
     def __init__(self,
                  temp_graph: pd.DataFrame,
-                 float_round_algorithm: AbstractFloatRoundAlgorithm
+                 weather_temp_round_algorithm: AbstractFloatRoundAlgorithm
                  ) -> None:
         self._logger = logging.getLogger(self.__class__.__name__)
         self._logger.debug("Creating instance")
 
         self._temp_graph = temp_graph
-        self._float_round_algorithm = float_round_algorithm
+        self._weather_temp_round_algo = weather_temp_round_algorithm
 
     def predict_on_weather(self, weather_df: pd.DataFrame) -> pd.DataFrame:
+        weather_df = self._round_weather_temp(weather_df)
+        temp_requirements_df = self._calc_for_weather_df(weather_df)
+        return temp_requirements_df
+
+    def _round_weather_temp(self, weather_df: pd.DataFrame) -> pd.DataFrame:
         weather_df = weather_df.copy()
         weather_df[column_names.WEATHER_TEMP] = \
-            self._float_round_algorithm.round_series(weather_df[column_names.WEATHER_TEMP])
+            self._weather_temp_round_algo.round_series(weather_df[column_names.WEATHER_TEMP])
+        return weather_df
+
+    def _calc_for_weather_df(self, weather_df: pd.DataFrame) -> pd.DataFrame:
         temp_requirements_list = []
         weather_temp_arr = weather_df[column_names.WEATHER_TEMP].to_numpy()
         timestamp_list = weather_df[column_names.TIMESTAMP].to_list()
         for weather_temp, timestamp in zip(weather_temp_arr, timestamp_list):
-            temp_requirements = self._calc_for_weather_temp(weather_temp)
-
-            required_forward_temp = temp_requirements[column_names.FORWARD_PIPE_COOLANT_TEMP]
-            required_backward_temp = temp_requirements[column_names.BACKWARD_PIPE_COOLANT_TEMP]
+            forward_temp, backward_temp = self._calc_for_weather_temp(weather_temp)
             temp_requirements_list.append({
                 column_names.TIMESTAMP: timestamp,
-                column_names.FORWARD_PIPE_COOLANT_TEMP: required_forward_temp,
-                column_names.BACKWARD_PIPE_COOLANT_TEMP: required_backward_temp
+                column_names.FORWARD_PIPE_COOLANT_TEMP: forward_temp,
+                column_names.BACKWARD_PIPE_COOLANT_TEMP: backward_temp
             })
-
         temp_requirements_df = pd.DataFrame(temp_requirements_list)
         return temp_requirements_df
 
-    def _calc_for_weather_temp(self, weather_temp: float) -> Dict[str, float]:
+    def _calc_for_weather_temp(self, weather_temp: float) -> Tuple[float, float]:
         available_temp = self._temp_graph[self._temp_graph[column_names.WEATHER_TEMP] <= weather_temp]
         if not available_temp.empty:
             required_temp_idx = available_temp[column_names.WEATHER_TEMP].idxmax()
@@ -50,10 +55,4 @@ class TempGraphRequirementsPredictor(AbstractTempRequirementsPredictor):
 
         forward_temp = self._temp_graph.loc[required_temp_idx, column_names.FORWARD_PIPE_COOLANT_TEMP]
         backward_temp = self._temp_graph.loc[required_temp_idx, column_names.BACKWARD_PIPE_COOLANT_TEMP]
-
-        required_temp = {
-            column_names.FORWARD_PIPE_COOLANT_TEMP: forward_temp,
-            column_names.BACKWARD_PIPE_COOLANT_TEMP: backward_temp
-        }
-
-        return required_temp
+        return forward_temp, backward_temp
