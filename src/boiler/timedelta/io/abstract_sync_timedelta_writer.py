ba4f07bfb1308bf20ef16ebde43d18b9e712cd1f
Georgij <netbay.georgij@gmail.com>
2021-05-12 03:59:37 +0500

    Фиксим разницу интерфейсов Timedelta IO

--- src/boiler/timedelta/io/abstract_sync_timedelta_writer.py
+++ src/boiler/timedelta/io/abstract_sync_timedelta_writer.py
@@ -7,5 +7,5 @@ class AbstractSyncTimedeltaWriter:
 
     def write_timedelta_to_binary_stream(self,
                                          binary_stream: BinaryIO,
-                                         temp_graph_df: pd.DataFrame) -> None:
+                                         timedelta_df: pd.DataFrame) -> None:
         raise NotImplementedError
