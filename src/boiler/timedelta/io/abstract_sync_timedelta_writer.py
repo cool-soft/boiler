be22e989a0c8a2ba8ac269acff8fa799a49c56ce
Georgij <netbay.georgij@gmail.com>
2021-05-12 04:29:19 +0500

    ~~~

--- src/boiler/timedelta/io/abstract_sync_timedelta_writer.py
+++ src/boiler/timedelta/io/abstract_sync_timedelta_writer.py
@@ -7,5 +7,6 @@ class AbstractSyncTimedeltaWriter:
 
     def write_timedelta_to_binary_stream(self,
                                          binary_stream: BinaryIO,
-                                         timedelta_df: pd.DataFrame) -> None:
+                                         timedelta_df: pd.DataFrame
+                                         ) -> None:
         raise NotImplementedError
