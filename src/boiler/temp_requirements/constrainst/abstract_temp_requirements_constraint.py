445dd5e35e0640833e1898040ed3ec340ca50e13
Georgij <netbay.georgij@gmail.com>
2021-05-12 03:52:18 +0500

    Фиксим разницу в параметрах интерфейса и реализации Constraint

--- src/boiler/temp_requirements/constrainst/abstract_temp_requirements_constraint.py
+++ src/boiler/temp_requirements/constrainst/abstract_temp_requirements_constraint.py
@@ -4,7 +4,7 @@ import pandas as pd
 class AbstractTempRequirementsConstraint:
 
     def check(self,
-              control_action_df: pd.DataFrame,
+              system_reaction_df: pd.DataFrame,
               weather_df: pd.DataFrame
               ) -> bool:
         raise NotImplementedError
